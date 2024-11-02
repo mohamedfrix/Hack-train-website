3d06a27e9fad81eb20d6f3c5cce85985c389025d
mohamedfrix <mohamed.frihaoui@ensia.edu.dz>
2024-11-02 11:39:35 +0100

    fix: remove unnecessary code

--- sec-ai-backend/app.py
+++ sec-ai-backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from leaderboard import LeaderBoard
 import json
-from supabase_testing import SupaBase
 
 app = Flask(__name__)
 
@@ -14,11 +13,6 @@ def leaderboard():
     leaderboard = LeaderBoard()
     return jsonify(leaderboard.get_kaggle_leaderboard())
 
-@app.route('/api/auth', methods=['GET'])
-def auth():
-    supabase = SupaBase()
-    response = supabase.sign_in()
-    return jsonify(vars(response))
 
 if __name__ == '__main__':
     app.run(debug=True)

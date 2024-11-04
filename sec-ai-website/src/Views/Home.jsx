833060ae4a0c94719784b196d9de2ad3afbfd842
ZakLr <z.lourghi@esi-sba.dz>
2024-11-04 21:40:59 +0100

    Revert "Merge branch 'development' of https://github.com/Su-mia/SecAi-event into development"
    This reverts commit 1560e274a80f37a3339956bfbe6a24a655ebc2cf, reversing
    changes made to 19a6415d43eae6b960f3d4c756ddbb804537a33d.

--- sec-ai-website/src/Views/Home.jsx
+++ sec-ai-website/src/Views/Home.jsx
@@ -1,24 +1,15 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Event from "../components/Event";
-import FAQ from "../components/FAQ";
-import MentorCarousel from "../components/Mentors";
-import Footer from "../components/Footer";
-import Agenda from "../components/Agenda";
+import React from 'react'
 
 function Home() {
   return (
-    <div className="font-fira-code">
-      <Navbar />
-      <Hero />
-      <Event />
-      <MentorCarousel />
-      <Agenda />
-      <FAQ />
-      <Footer />
+    <div>
+      home
+      <div className="font-open-sans">
+      <p>This is using Open Sans.</p>
+      <code className="font-fira-code">This is using Fira Code for code blocks.</code>
+     </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home

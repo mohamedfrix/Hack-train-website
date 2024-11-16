07645e464ade1a669c778d1eea8d8d3c1d268051
Su-mia <soumia.bouaouina@ensia.edu.dz>
2024-11-16 11:11:23 +0100

    pushing updatesS

--- sec-ai-website/src/Components/Home/Hero.jsx
+++ sec-ai-website/src/Components/Home/Hero.jsx
@@ -6,7 +6,7 @@ import Binaries from "../../assets/Home/hero-binaries.svg";
 import MagicalButton from "./MagicalButton";
 
 export default function Hero() {
-  const targetDate = new Date("2024-11-31T00:00:00").getTime(); // Set your target date here
+  const targetDate = new Date(Date.UTC(2024, 10, 30, 0, 0, 0));
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {

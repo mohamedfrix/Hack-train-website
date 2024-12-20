ac6db46159a32c88537024fc1f37f2815502d614
mohamedfrix <mohamed.frihaoui@ensia.edu.dz>
2024-12-20 04:50:21 +0100

    fix

--- sec-ai-website/vite.config.js
+++ sec-ai-website/vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://hachtrainbackend.onrender.com', // Replace with your API server's URL
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+
 })
